25fca1f6e1c411185a57fcc58a5817c92aa4b6fe
affable-motherboard-2531 <solomonrfeldman@gmail.com>
2019-03-20 10:43:55 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,5 @@
 # code the #greeting method here!
-def greeting(name)
+def greeting()
   puts "Greetings Traveler, I'm Rin. What is your name?"
   name = gets.strip
   puts "Hello #{name}. It's nice to meet you."
